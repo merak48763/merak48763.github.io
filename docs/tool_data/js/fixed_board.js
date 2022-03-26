09a761112903031d93497d9aa92dc0587fa12485
Merak <a564178239@gmail.com>
2022-03-26 15:33:33 +0800

    Added fixed_shape function

--- docs/tool_data/js/fixed_board.js
+++ docs/tool_data/js/fixed_board.js
@@ -1,4 +1,4 @@
-function get_fixed_board(board_rep) {
+function get_fixed_shape(args) {
     // 2 digits for one position
     // first digit: attribute | enchanted<<3
     // second digit: race
@@ -7,12 +7,15 @@ function get_fixed_board(board_rep) {
     // 0 1 2 3 4 5
     // 6 7 ...
     // ...
-    let board = Array.from({length: 5}, (v, k) => Array.from({length: 6}, (v, k) => 0));
-    const rune_types = ['none', 'water', 'fire', 'earth', 'light', 'dark', 'heart'];
+    let nr, nc, board_repr;
+    [nr, nc, board_repr] = args.split(' ');
+
+    let board = Array.from({length: nr}, () => Array.from({length: nc}, () => 0));
+    const rune_types = ['none', 'water', 'fire', 'earth', 'light', 'dark', 'heart', 'lock'];
     const rune_races = ['none', 'human', 'god', 'demon', 'dragon', 'beast', 'elf', 'machina'];
-    for(let row=0; row<5; ++row) for(let col=0; col<6; ++col) {
-        const type_n = parseInt(board_rep[2*(6*row+col)], 16);
-        const race_n = parseInt(board_rep[2*(6*row+col)+1], 16);
+    for(let row=0; row<nr; ++row) for(let col=0; col<nc; ++col) {
+        const type_n = parseInt(board_repr[2*(nc*row+col)], 16);
+        const race_n = parseInt(board_repr[2*(nc*row+col)+1], 16);
         board[row][col] = {
             'type': rune_types[type_n&7] + (type_n>8?'_e':''),
             'race': rune_races[race_n]
@@ -21,9 +24,9 @@ function get_fixed_board(board_rep) {
 
     const image_base = '/tool_data/image/rune/';
     let board_html = '<table class="fixed_board">';
-    for(let row=0; row<5; ++row) {
+    for(let row=0; row<nr; ++row) {
         board_html += '<tr>';
-        for(let col=0; col<6; ++col) {
+        for(let col=0; col<nc; ++col) {
             board_html += `<td><img src="${image_base + board[row][col].type}.png" /><img src="${image_base + board[row][col].race}.png" /></td>`;
         }
         board_html += '</tr>';
@@ -31,4 +34,21 @@ function get_fixed_board(board_rep) {
     board_html += '</table>';
 
     return board_html;
+}
+
+function get_fixed_board(board_repr) {
+    // fixed 5r*6c
+    return get_fixed_shape('5 6 ' + board_repr);
+}
+
+function get_fixed_pos(pos_repr) {
+    // fixed 5r*6c
+    let board_repr = '';
+    [...pos_repr].forEach((mark) => {
+        if(mark == '1') board_repr += '70';
+        else if(mark == '2') board_repr += 'f0';
+        else board_repr += '00';
+    });
+    
+    return get_fixed_shape('5 6 ' + board_repr);
 }
