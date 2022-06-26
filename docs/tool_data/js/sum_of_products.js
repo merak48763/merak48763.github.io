a1a8f920f62397c6b4ea0c98f7ac5865922b9a31
Merak <a564178239@gmail.com>
2022-06-26 09:05:40 +0800

    Added common script of keyword searching

---
+++ docs/tool_data/js/sum_of_products.js
@@ -0,0 +1,16 @@
+function createSOPList(rawStr) {
+    let result = rawStr.split(' ');
+    result.forEach((element, index) => {
+        result[index] = element.split('&');
+    });
+    return result;
+}
+
+function reduceSOPList(SOPList) {
+    let result = structuredClone(SOPList);
+    result.forEach((element, index) => {
+        result[index] = element.reduce((partialProd, currentValue) => partialProd.filter(x => currentValue.includes(x)), []);
+    });
+    result = result.reduce((partialSum, currentValue) => partialSum.concat(currentValue.filter(x => !partialSum.includes(x))), []);
+    return result;
+}
