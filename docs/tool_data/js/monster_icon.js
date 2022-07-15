97720f1a6a726833b43b6dec8b48b9ecd6aed317
Merak <a564178239@gmail.com>
2022-07-15 22:39:55 +0800

    Added monster data

--- docs/tool_data/js/monster_icon.js
+++ docs/tool_data/js/monster_icon.js
@@ -1,7 +1,19 @@
 let monsterData = null;
 async function loadMonsterData() {
     if(monsterData === null) {
-        // ...
+        monsterData = await fetch('/tool_data/data/monster.json').then(res => {
+            if(res.status == 200) {
+                return res.json();
+            }
+            return {
+                'last_update': {
+                    'version': '-',
+                    'dv': '-',
+                    'time': 0
+                },
+                'monster': {}
+            };
+        });
     }
 }
 
@@ -14,12 +26,18 @@ const generateTooltipId = (function() {
 }());
 
 function generateMonsterIcon(monsterId) {
-    const image_base = 'https://tinghan33704.github.io/tos_tool_data/img/monster/';
-    //const image_base = '/tool_data/image/monster/';
+    const image_base = '/tool_data/image/monster/';
     const tooltipId = generateTooltipId();
     return {
-        'html': `<img src="${image_base}${monsterId}.png" alt="${monsterId}" data-tooltip-id="${tooltipId}" /><div class="mdc-tooltip" id="${tooltipId}" role="tooltip" aria-hidden="true"><div class="mdc-tooltip__surface mdc-tooltip__surface-animation">#${monsterId}</div></div>`,
+        'html': `<img src="${image_base}${monsterId}.png" alt="${monsterId}" data-tooltip-id="${tooltipId}" /><div class="mdc-tooltip" id="${tooltipId}" role="tooltip" aria-hidden="true"><div class="mdc-tooltip__surface mdc-tooltip__surface-animation">#${monsterId} ${getMonsterName(monsterId)}</div></div>`,
         'tooltipId': tooltipId
     };
     // mdc.tooltip.MDCTooltip.attachTo(document.querySelector('div#'+result.tooltipId));
+}
+
+function getMonsterName(monsterId) {
+    if(monsterId in monsterData) {
+        return monsterData[monsterId].name;
+    }
+    return null;
 }
